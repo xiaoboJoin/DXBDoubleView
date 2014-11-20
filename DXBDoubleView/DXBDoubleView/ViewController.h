ce83af10ba80ed229bfad38bb9ee9aedd617948f
xiaoboJoin <xiaobo_duan@126.com>
2014-11-20 13:41:14 +0800

    initial

---
+++ DXBDoubleView/DXBDoubleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DXBDoubleView
+//
+//  Created by iXiaobo on 14-11-20.
+//  Copyright (c) 2014年 iXiaobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
